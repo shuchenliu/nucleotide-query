730563711578e80565bb39ba60603040631ca1b3
shuchenliu <shuchenliu2014@gmail.com>
2025-06-02 11:34:06 -0700

    add validator to Match serializer

--- nucleotide_query/api/serializers.py
+++ nucleotide_query/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 
-from nucleotide_query.api.models import Match
+from nucleotide_query.api.models import Match, SearchTerm
 
 
 class MatchSerializer(serializers.ModelSerializer):
@@ -8,4 +9,12 @@ class MatchSerializer(serializers.ModelSerializer):
         model = Match
         fields = ['id', 'start', 'end']
 
+        # enforce start-end unique combination early on
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Match.objects.all(),
+                fields=('start', 'end'),
+                message='This combination of start and end must be unique'
+            )
+        ]
 
