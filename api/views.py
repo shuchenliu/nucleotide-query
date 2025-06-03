1f98e96baf7ec75fabc86e0128a3a7c899d3fefd
shuchenliu <shuchenliu2014@gmail.com>
2025-06-02 17:29:34 -0700

    add and register Query view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from api.serializers import SearchTermSerializer
+
+
+class QueryView(APIView):
+    parser_classes = [JSONParser]
+
+    def post(self, request):
+
+        # use serializer to conduct validation
+        serializer = SearchTermSerializer(data=request.data)
+
+        # handle invalid requests
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
