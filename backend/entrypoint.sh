d7e4b83fae911c53667c31462fef04d39252ed88
shuchenliu <shuchenliu2014@gmail.com>
2025-06-05 15:54:40 -0700

    update docker scripts

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+python manage.py makemigrations
+python manage.py migrate
 python manage.py preload_reference
 
 if [ "$DJANGO_ENV" = "production" ]; then
-  gunicorn project.wsgi:application --bind 0.0.0.0:8000
+  echo "running in $DJANGO_ENV mode"
+  gunicorn nucleotide_query.wsgi:application --bind 0.0.0.0:8000
 else
+  echo "running in non production mode"
   python manage.py runserver 0.0.0.0:8000
 fi
