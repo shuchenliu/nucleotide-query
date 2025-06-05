7085143740448e82200f544ee3d5a8a1d1b1ebb6
shuchenliu <shuchenliu2014@gmail.com>
2025-06-04 18:15:15 -0700

    fix pagination to include ?page=1 in references

--- api/views/query/pagination.py
+++ api/views/query/pagination.py
@@ -1,6 +1,23 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.utils.urls import replace_query_param
+
 
 class QueryPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 500
+
+    def get_previous_link(self):
+        """
+        Override get_previous_link to enforce `?page=1` for the first page in reference.
+        This is helpful since we rely on the existence of page param to decide the timing to save a search
+
+        :return: url pointing to previous page
+        """
+        if not self.page.has_previous():
+            return None
+
+        url = self.request.build_absolute_uri()
+        page_number = self.page.previous_page_number()
+
+        return replace_query_param(url, self.page_query_param, page_number)
