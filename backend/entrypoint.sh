232ef693a03fe2d7e07e2fc0e37c193e42bf662b
shuchenliu <shuchenliu2014@gmail.com>
2025-06-05 14:40:42 -0700

    add redis cache

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,5 @@ python manage.py preload_reference
 if [ "$DJANGO_ENV" = "production" ]; then
   gunicorn project.wsgi:application --bind 0.0.0.0:8000
 else
-  echo "hey"
   python manage.py runserver 0.0.0.0:8000
 fi
--- backend/nucleotide_query/settings.py
+++ backend/nucleotide_query/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redis as  cache backend
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379",
+    }
+}
