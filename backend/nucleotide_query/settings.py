aa539dd59986c7ff1786e017691e45f0ad8639e1
shuchenliu <shuchenliu2014@gmail.com>
2025-06-05 13:14:57 -0700

    move backend project to prepare for multi-container setup

--- backend/nucleotide_query/settings.py
+++ backend/nucleotide_query/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'nucleotide_query.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'backend/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
